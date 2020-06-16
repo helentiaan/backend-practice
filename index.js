c1975d24f6c5914273cf20a18da0a66fa796a2a1
Helen Tian <43627816+bb0000@users.noreply.github.com>
2020-06-16 10:40:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT, 
+    { useNewUrlParser: true},
+    ()=> console.log('connected to db!'));
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+
+//Route Middleware
+app.use('/api/user', authRoute);
+
+
+app.listen(3000, ()=>console.log("server up and running"));
